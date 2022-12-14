b79a8d43f4f26cfcd96e4ad4ea5a685f1babd410
Alexis <alexis.ortiz@wwit.cl>
2022-12-14 00:09:37 -0300

    Remove lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import HeaderComponent from './Components/header'
-import Example from './Components/Tab'
-import ExampleHeader from './Components/Headers'
+import ExampleHeaders from './Components/Headers'
 import Hero from './Components/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <HeaderComponent />
       <div className="App">
-        {/* <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div> */}
-
-        <ExampleHeader></ExampleHeader>
-
+        <ExampleHeaders />
         <Hero />
       </div>
     </>
